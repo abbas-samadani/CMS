40caaef1cf79e913ce8e1b0eaeb9b2d774b225a8
Abbas <a.samadani256@gmail.com>
2021-03-11 21:15:23 +0000

    complete add menu section

--- include/functions.php
+++ include/functions.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+session_start();
     function config(){
         $server="localhost";
         $user="root";
@@ -9,4 +9,27 @@
 
         return $connect;
     }
+
+    function addmenu($data){
+        var_dump($data);
+        $connection = config();
+        $sql = "INSERT INTO menu_tbl (title,url,status,chid) VALUES ('$data[title]','$data[url]','$data[status]','$data[parent]')";
+        $row = mysqli_query($connection,$sql);
+        //$res = mysqli_fetch_array($row);
+        //var_dump($res);
+    }
+
+    function submenu(){
+        $connection = config();
+        $sql = "SELECT * FROM menu_tbl WHERE chid='0'";
+        $row = mysqli_query($connection,$sql);
+        //$res = mysqli_fetch_array($row);
+        //var_dump($res);
+        while ($res = mysqli_fetch_array($row)){
+            $result[]=$res;
+        }
+        return $result;
+    }
+
+    submenu();
 
